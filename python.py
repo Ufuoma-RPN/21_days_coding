5ecc092c9e017aea179457e3b85deaa17e9cf150
Ufuoma-RPN <ogheneteganiameh@gmail.com>
2020-06-15 17:51:43 +0100

    edit python and add

--- python.py
+++ python.py
@@ -1,4 +1,12 @@
-name = "Ufuoma"
-location = "No 18, Arinola Street, Oriokuta Street, Ikorodu, Lagos"
-email = "ogheneteganiameh@gmail.com"
-print(name, location, email)
+# Python program to find the perimeter of a rectangle
+# 1. Take length and breadth of rectangle
+# 2. Calculate perimeter of rectangle
+# 3. Display calculated perimeter on screen
+length = 5
+breadth = 15
+print("length is", length)
+print("breadth is", breadth)
+# Calculate the perimeter = 2 * (length + breadth)
+perimeter = 2 * (length + breadth)
+
+print("Perimeter of rectangle is", perimeter)
